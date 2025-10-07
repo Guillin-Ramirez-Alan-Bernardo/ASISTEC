c4b2d3043f881dbf483d6bc03192536b1f164ef0
unknown <a21490078@itmexicali.edu.mx>
2025-10-06 21:48:55 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,189 @@
+import 'package:asistec_b/Second_S.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final bool showLogoutMessage; //Los elementos que queria que se vean en la pantalla, aqui se ingresan primero
+  const MyApp({super.key, this.showLogoutMessage =  false}); //Luego se registran aqui, pero no se registra dentro de la clase con los elementos
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ASISTEC',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 3, 11, 234)),
+      ),
+      home: MyHomePage(title: '',
+      showLogoutMessage: showLogoutMessage,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title, this.showLogoutMessage = false,});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+  final bool showLogoutMessage;  //Este showLogoutMesse es el que define en toda la clase, aparte de los demas
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool passwordVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Mostrar SnackBar al entrar, si viene del logout
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (widget.showLogoutMessage) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text(
+              'Se ha cerrado la sesión correctamente',
+              style: TextStyle(fontSize: 14, color: Colors.black),
+            ),
+            backgroundColor: Colors.blue,
+            duration: Duration(seconds: 4),
+            behavior: SnackBarBehavior.floating,
+            margin: EdgeInsets.all(16),
+          ),
+        );
+      }
+    });
+  }  
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+
+        children: [
+            Image.asset(
+            'assets/imagenes/tec1.png',
+                width: 400,
+                height: 200,
+            ),
+
+
+          const SizedBox(height: 10),
+            
+            TextField(
+              decoration: const InputDecoration(
+              border: const OutlineInputBorder(),
+              labelText: 'Número de control',
+              ),
+            ),
+
+          const SizedBox(height: 40),
+            
+            TextField(
+              obscureText: passwordVisible,
+              decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              hintText: "Contraseña",
+              labelText: "Contraseña",
+              helperStyle: const TextStyle(color: Colors.green),
+              suffixIcon: IconButton(
+              icon: Icon(
+                passwordVisible ? Icons.visibility : Icons.visibility_off,
+              ),
+                onPressed: () {
+                  setState(() {
+                    passwordVisible = !passwordVisible;
+                  });
+                },
+              ),
+            ),
+          ),
+          
+          const SizedBox(height: 10),
+          ElevatedButton(
+             onPressed: () {
+               // Acción que se ejecuta al presionar el botón
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                builder: (context) => const SecondS(),
+                ),
+              );
+            },
+            child: const Text("Iniciar sesion")
+            ),
+
+          const SizedBox(height: 10),
+          Image.asset(
+            'assets/imagenes/Lo.jpg',
+              width: 200,
+              height: 150,
+          ),
+
+        ],
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+        ),
+      ),    
+    );
+  }
+}
---
+++ lib/second_s.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:asistec_b/main.dart';
+
+class SecondS extends StatelessWidget {
+  const SecondS({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+          return Scaffold(
+            appBar: 
+            AppBar(title: const Text(''),
+            automaticallyImplyLeading: true,
+            backgroundColor: const Color.fromARGB(255, 30, 77, 245),
+
+            ),
+              drawer: Drawer(
+                child: ListView(
+                  children: [
+                    const DrawerHeader(
+                      decoration: BoxDecoration(color: Colors.blue),
+                      child: Text('Menú principal', style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold), ),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.home),
+                      title: Text('Inicio'),
+                      onTap: (){
+                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const SecondS()
+                          )
+                        );
+                      },
+                    ),
+                    const ExpansionTile(
+                      leading: Icon(Icons.people),
+                      title: Text('Personal'),
+                      children: [
+                        ListTile(title: Text('Alta de personal')),
+                        ListTile(title: Text('Baja de personal')),
+                      ],
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.logout),
+                      title: Text('Salir'),
+                      onTap: () {
+                        Navigator.pushReplacement(context, MaterialPageRoute(builder:(context) => const MyApp(showLogoutMessage: true,)
+                          )
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+      body: Center(
+        
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Text('¡Bienvenid@ ________________ Que te Gustaria consultar el dia de hoy!',
+            style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+            ),
+            
+            const SizedBox(height: 20),
+          
+            TableCalendar(
+              focusedDay: DateTime.now(), 
+              firstDay: DateTime.utc(2025,10,1), 
+              lastDay: DateTime.utc(2030,1,1),
+              calendarFormat: CalendarFormat.month,
+              onDaySelected: (selectedDay, focusedDay) 
+                { print('Seleccionaste: $selectedDay'); 
+            
+              },
+            ),
+            const SizedBox(height: 10),
+            Image.asset(
+              'assets/imagenes/Lo.jpg',
+                width: 200,
+                height: 150,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
